eec2a5abb5a6033ad2bbc365e5600a75144d8416
Geon gyu Park <geongyupark@GeonuiMBookAir2.kornet>
2022-12-21 11:17:57 +0900

    fix: Fix applied pydantic return value
    파이단틱 적용사항 수정

--- fastapi_test/pydantic_test.py
+++ fastapi_test/pydantic_test.py
@@ -1,9 +1,10 @@
+import json
 from typing import Optional, Union
 
 import uvicorn
 
 from fastapi import FastAPI, status
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, Response
 from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
@@ -38,19 +39,33 @@ class PostResponse(BaseModel):
   option1 : Optional[int] # Union 사용해도 가능
   option2 : Union[int, None]
 
+class Count(BaseModel):
+  counts : int
+
 fake_db = {
     "id" : 1,
     "name" : "test",
-    "password" : "test1234!" 
+    "password" : "test1234!",
+    "image_url" : "https://test.img/test1.jpg"
 }
 
 @app.get('/', response_model=ResponseTest, status_code=200)
 async def get_fake_user():
     '''
-    Simple Dict value return and JSONResponse content return are Possible
+    reponse_model을 적용할 때는 단순 return으로 진행해야 schema에서 지정한 형식으로 필터링할 수 있다.
+    JSONResponse나 Response 등을 사용하게 된다면, schema를 거치지 않고 전달된다.
     '''
-    # return fake_db
-    return JSONResponse(content=fake_db, status_code=status.HTTP_200_OK)
+    return fake_db
+    # return Response(content=json.dumps(fake_db))
+    # return JSONResponse(content=fake_db, status_code=status.HTTP_200_OK)
+  
+@app.get('/counts', response_model=Count)
+async def get_fake_counts():
+  data = {
+    'counts' : 1
+  }
+
+  return JSONResponse(content=data, status_code=status.HTTP_200_OK)
 
 @app.post('/')
 async def rank_user(request : User):
